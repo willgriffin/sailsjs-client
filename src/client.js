59b46e1973babdf4f648c55d91e4bc4fe7e2d311
will griffin <willgriffin@gmail.com>
2019-04-17 04:29:01 -0700

    initial commit

---
+++ src/client.js
@@ -0,0 +1,205 @@
+import request from 'request'
+import SocketIo from 'socket.io-client'
+import SailsIo from 'sails.io.js'
+import { URL } from 'url';
+
+class SailsClient {
+	
+	constructor(args) {
+		this.url = new URL(args.url || 'http://localhost:1337/api/')
+		if (this.url.protocol === null) {
+			throw new Error('Invalid url');
+		}
+		if (args.auth) {
+			this.auth = {
+				username: args.auth.username,
+				password: args.auth.password,
+				endpoint: args.auth.endpoint || 'auth/token',
+				usernameField: args.auth.usernameField || 'email',
+				passwordField: args.auth.passwordField || 'password'
+			}
+		}
+		this.reconnect = true;
+		this.listeners = [];
+	}
+	
+	async init() {
+		await this.getCookie()
+		await this.socketInit()
+		await this.connect()
+		if (this.auth) {
+			await this.authenticate()
+		}
+	}
+	
+	on(event, method) {
+		this.listeners.push({
+			event, method
+		})
+		if (this.isConnected() === true) {
+			this.socket.on(event, method)
+		}
+	}
+
+	async getCookie() {
+		const response = await this.request({
+			uri: this.url.origin + '/__getcookie',
+			method: 'GET'
+		})
+		this.cookies = response.headers['set-cookie']
+	}
+
+	async authenticate(auth) {
+		const response = await this.post(auth.endpoint, {
+			[auth.usernameField]: auth.username,
+			[auth.passwordField]: auth.password
+		})
+		auth.response = response
+		return auth
+	}
+
+	async get(path, params) {
+		const response = await this.request({
+			uri: this.url.href + path,
+			method: 'GET',
+			body: params,
+			json: true
+		})
+		return response.body
+	}
+	
+	async post(path, params) {
+		const response = await this.request({
+			uri: this.url.href + path,
+			method: 'POST',
+			body: params,
+			json: true
+		})
+		return response.body
+	}
+
+	async put(path, params) {
+		const response = await this.request({
+			uri: this.url.href + path,
+			method: 'PUT',
+			body: params,
+			json: true
+		})
+		return response.body
+	}
+
+	async patch(path, params) {
+		const response = await this.request({
+			uri: this.url.href + path,
+			method: 'PATCH',
+			body: params,
+			json: true
+		})
+		return response.body
+	}
+
+	async delete(path, params) {
+		const response = await this.request({
+			uri: this.url.href + path,
+			method: 'DELETE',
+			body: params,
+			json: true
+		})
+		return response.body
+	}
+
+	async request(params) {
+		let response
+		if (this.isConnected() === true) {
+			response = await this.socketRequest(params)
+		} else {
+			response = await this.xhrRequest(params)
+		}
+		if (response.statusCode !== 200) {
+			throw new Error(`[${response.statusCode}] ${response.body}`);
+		}
+		return response
+	}
+
+	async socketInit() {
+		this.io = new SailsIo(SocketIo)
+		this.io.sails.initialConnectionHeaders = {
+			'cookie': this.cookies,
+			//'Authorization': this.auth && this.auth.token
+		}
+		this.io.sails.url = this.url.origin
+		this.io.sails.autoConnect = false;
+		this.io.sails.reconnection = this.reconnect
+	}
+
+	isConnected() {
+		if (typeof this.socket === 'undefined') {
+			return false
+		} else {
+			return this.socket.isConnected()
+		}
+	}
+
+	async connect() {
+		if (this.isConnected() === false) {
+			return new Promise((resolve, reject) => {
+				this.socket = this.io.sails.connect()
+				for (const listener of this.listeners) {
+					this.socket.on(listener.event, listener.method)
+				}
+				this.socket.on('connect', (data) => {
+					resolve(true)
+				})
+			})
+		} else {
+			console.warn('Already connected')
+		}
+	}
+
+	async disconnect() {
+		if (this.isConnected() === true) {
+			return new Promise((resolve, reject) => {
+				this.socket.on('disconnect', async (data) => {
+					resolve(true)
+				})
+				this.socket.disconnect()
+			})
+		} else {
+			console.warn('already disconnected')
+		}
+	}
+
+	socketRequest(params) {
+		return new Promise((resolve, reject) => {
+			if (typeof params.headers === 'undefined') {
+				params.headers = {}
+			}
+			try {
+				this.socket.request({
+					method: params.method || 'GET',
+					url: params.uri,
+					data: params.body,
+					headers: params.headers
+				}, function (body, jwr) {
+					return resolve(jwr)
+				})
+			} catch (err) {
+				return reject(err)
+			}
+		})
+	}
+
+	xhrRequest(params) {
+		return new Promise((resolve, reject) => {
+			request(params, function (err, response, body) {
+				if (err)
+					return reject(err)
+				else {
+					return resolve(response)
+				}
+			})
+		})
+	}
+}
+
+export default SailsClient
---
+++ test/unit/client.spec.js
@@ -0,0 +1,164 @@
+
+import Sails from 'sails'
+import SHSockets from 'sails-hook-sockets'
+import jwt from 'jsonwebtoken'
+import { expect } from 'chai';  // Using Expect style
+import SailsClient from '../../src/client.js'
+
+
+let api, app;
+
+describe('client.spec.js', () => {
+	
+	before(() => {
+
+		let ok = new Promise((resolve, reject) => {
+			app = new Sails.constructor()
+			app.lift({
+				log: {
+					level: 'silent'
+				},
+				globals: {
+					sails: true,
+					_: false,
+					async: false,
+					models: false,
+					globals: false
+				},
+				port: 13666,
+				sockets: {
+					transports: ['websocket']
+				},
+				hooks: {
+					grunt: false,
+					sockets: SHSockets
+				},
+				session: { 
+					secret: 'poopityscoop'
+				}
+			}, function (err) {
+				if (err) {
+					reject(err)
+				}
+				resolve(true)
+			})
+		})
+		ok = ok.then(() => {
+			const requestEndpoint = (req, res) => {
+				return res.json({
+					url: req.url,
+					method: req.method,
+					isSocket: req.isSocket,
+					headers: req.headers,
+				})
+			}
+
+			app.get('/api/request', requestEndpoint)
+			app.post('/api/request', requestEndpoint)
+			app.put('/api/request', requestEndpoint)
+			app.delete('/api/request', requestEndpoint)
+
+			app.post('/api/socket/join', (req, res) => {
+				if (!req.isSocket) {
+					return res.badRequest();
+				}
+				const roomName = req.param('room');
+				sails.sockets.join(req, roomName, (err) => {
+					if (err) {
+						return res.serverError(err);
+					}
+					return res.json({
+						message: 'Joined ' + roomName
+					});
+				});
+			})
+
+			app.post('/api/socket/blast', (req, res) => {
+				app.sockets.blast('message', req.param('message'));
+			})
+
+			app.post('/api/auth/token', (req, res) => {
+				if (req.param('email') === 'test@user.com' && req.param('password') === 'password123') {
+					const token = jwt.sign({
+						foo: 'bar'
+					}, 
+					'supersecurekeygoeshere', 
+					{
+						expiresIn: '1h'
+					})
+					res.json({
+						token
+					})
+				} else {
+					res.forbidden()
+				}
+			})
+		})
+		ok = ok.then(() => {
+			api = new SailsClient({
+				url: 'http://localhost:13666/api/'
+			})
+			return api.init()
+		})
+		return ok;
+	});
+	
+	it('should able to make an xhr request', async () => {
+		const response = await api.xhrRequest({
+			uri: 'http://localhost:13666/api/request',
+			method: 'GET',
+			// body: params,
+			json: true
+		})
+		expect(!response.body.isSocket).to.equal(true)
+		expect(response.body.method).to.equal('GET')
+	})
+	
+	it('should be able to make a post request whether the socket is connected or not', async () => {
+
+		await api.disconnect()
+		const responseXhr = await api.post('request')
+		expect(responseXhr.method).to.equal('POST')
+		expect(!responseXhr.isSocket).to.equal(true)
+
+		await api.connect()
+		const response3 = await api.post('request')
+		expect(response3.method).to.equal('POST')
+		expect(response3.isSocket).to.equal(true)
+
+	})
+
+	it('should be able to make a get request whether the socket is connected or not', async () => {
+
+		await api.disconnect()
+		const responseXhr = await api.get('request')
+		expect(responseXhr.method).to.equal('GET')
+		expect(!responseXhr.isSocket).to.equal(true)
+
+		await api.connect()
+		const response3 = await api.get('request')
+		expect(response3.method).to.equal('GET')
+		expect(response3.isSocket).to.equal(true)
+
+	})
+
+	it('should be able to add event listeners to the socket', async () => {
+		//disconnect so there's no socket to test it's adding events in listeners array
+		await api.disconnect()
+		expect(api.isConnected()).to.equal(false)
+		await new Promise((resolve, reject) => {
+			api.on('message', (data) => {
+				expect(data).to.equal('foo')
+				if (data === 'foo') {
+					resolve()
+				}
+			})
+			api.connect().then(() => {
+				expect(api.isConnected()).to.equal(true)
+				api.post('socket/blast', {
+					message: 'foo'
+				})
+			})
+		})
+	})
+})
